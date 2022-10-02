f6de3be2156e91bd5b1a2fbe5042b1f6da34afaa
Andrew Farrell <atf8760@gmail.com>
2022-10-02 10:28:31 -0400

    Update extension.ts
    Visual changes to code organization

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,6 @@ import * as vscode from 'vscode';
 const date = require('date-and-time');
 
 export function activate(context: vscode.ExtensionContext) {
-    let msgOpts = {
-        detail: "Your timer complete.",
-        modal: true,
-    };
     
     // format to print out the final clock
     let format = 'HH:mm:ss';
@@ -24,7 +20,13 @@ export function activate(context: vscode.ExtensionContext) {
     barClock.text = date.format(new Date(),format);
     barClock.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
     barClock.show();
-
+	
+    // variable to configure the timer completion notification
+    let msgOpts = {
+        detail: "Your timer complete.",
+        modal: true,
+    };
+	
     // this will be used to update both the clock and timer on a 1000ms interval
     setInterval(function () {
         let now = new Date();
@@ -35,45 +37,42 @@ export function activate(context: vscode.ExtensionContext) {
             if (timerEnd > now) {
               // set the clock timer text to the differenec between the time 
               // stored in now and the time stored in the timer end.
-              barClock.text = date.format(new Date(0,0,0,
-                                            timerEnd.getHours() - now.getHours(),
-                                             timerEnd.getMinutes() - now.getMinutes(),
-                                              timerEnd.getSeconds() - now.getSeconds()),
-                                               format);
+                barClock.text = date.format(new Date(0,0,0,
+                    timerEnd.getHours() - now.getHours(),
+                    timerEnd.getMinutes() - now.getMinutes(),
+                    timerEnd.getSeconds() - now.getSeconds()),
+                    format);
             } else {
-              clockIsOn = true;
-              vscode.window.showInformationMessage("Timer complete", msgOpts);
+		// turn the clock back on and notify the user
+                clockIsOn = true;
+                vscode.window.showInformationMessage("Timer complete", msgOpts);
             }
         }}, 1000);
 
-    // set a command that will activate on clicking the clock widget
-    barClock.command = 'clock.timer';
+    	// set a command that will activate on clicking the clock widget
+    	barClock.command = 'clock.timer';
 	let disposable = vscode.commands.registerCommand('clock.timer', async () => {
-        let options = {
-            prompt: "Enter timer length or press esc to cancel",
-            placeHolder: "Please only use HH:MM:ss format",
-            value: context.globalState.get("previousTimer", "")
-        };
-        
-        let ret = await vscode.window.showInputBox(options);
-        
-        console.log("Input: " + ret);
-        context.globalState.update("previousTimer", ret);
-        if (ret) {
-    
-            // **No thorough checking is done of the input, I only check
-            // to make sure the string has something in it
-            let [hours, minutes, seconds] = ret.split(':');
-            timerEnd = new Date();
-            timerEnd.setHours(timerEnd.getHours() + +hours,
-                              timerEnd.getMinutes() + +minutes,
-                              timerEnd.getSeconds() + +seconds);
-            clockIsOn = false;
-            vscode.window.showInformationMessage("Timer started");
-
-            // console.log("Timer is set to end at: " + timerEnd);
-            // console.log(context.globalStorageUri);
-        }
+		let options = {
+		    prompt: "Enter timer length or press esc to cancel",
+		    placeHolder: "Please only use HH:MM:ss format",
+		    value: context.globalState.get("previousTimer", "")
+		};
+		
+		// get and set user input
+		let ret = await vscode.window.showInputBox(options);
+		context.globalState.update("previousTimer", ret);
+		
+		if (ret) {
+		    // **No thorough checking is done of the input, I only check
+		    // to make sure the string has something in it
+		    let [hours, minutes, seconds] = ret.split(':');
+		    timerEnd = new Date();
+		    timerEnd.setHours(timerEnd.getHours() + +hours,
+				      timerEnd.getMinutes() + +minutes,
+				      timerEnd.getSeconds() + +seconds);
+		    clockIsOn = false;
+		    vscode.window.showInformationMessage("Timer started");
+		}
 	});
 
 	context.subscriptions.push(disposable);
