93179f27db98737ac2246bf70417599683bbae6a
Andrew Farrell <atf8760@gmail.com>
2022-10-02 10:38:07 -0400

    visual changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,16 @@
-
 import * as vscode from 'vscode';
 const date = require('date-and-time');
 
 export function activate(context: vscode.ExtensionContext) {
-    
     // format to print out the final clock
     let format = 'HH:mm:ss';
     let clockIsOn = true;
-    
     
     // This will be used to store the end of timer data
     // I'm declaring it here because I'll need it in different parts of the code
     // later. Not ideal but it is what it is. 
     let timerEnd: Date; 
 
-    
     // set up the clock and make it show up
     let barClock = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
     barClock.text = date.format(new Date(),format);
@@ -35,22 +31,23 @@ export function activate(context: vscode.ExtensionContext) {
         } else {
             // if the timer hasn't ended yet
             if (timerEnd > now) {
-              // set the clock timer text to the differenec between the time 
-              // stored in now and the time stored in the timer end.
+                // set the clock timer text to the differenec between the time 
+                // stored in now and the time stored in the timer end.
                 barClock.text = date.format(new Date(0,0,0,
                     timerEnd.getHours() - now.getHours(),
                     timerEnd.getMinutes() - now.getMinutes(),
                     timerEnd.getSeconds() - now.getSeconds()),
                     format);
             } else {
-		// turn the clock back on and notify the user
+		        // turn the clock back on and notify the user
                 clockIsOn = true;
                 vscode.window.showInformationMessage("Timer complete", msgOpts);
             }
-        }}, 1000);
+        }
+    }, 1000);
 
-    	// set a command that will activate on clicking the clock widget
-    	barClock.command = 'clock.timer';
+    // set a command that will activate on clicking the clock widget
+    barClock.command = 'clock.timer';
 	let disposable = vscode.commands.registerCommand('clock.timer', async () => {
 		let options = {
 		    prompt: "Enter timer length or press esc to cancel",
@@ -74,9 +71,7 @@ export function activate(context: vscode.ExtensionContext) {
 		    vscode.window.showInformationMessage("Timer started");
 		}
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
